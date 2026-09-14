f8c3f819a66b849ec92864f41b46ac3395319a26
Ritesh Saxena <kingriteshsa@gmail.com>
2026-09-14 07:17:55 +0530

    feat: add ApiError, ApiResponse, and asyncHandler utility classes

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,9 @@
+export const asyncHandler = (requestHandler) => {
+  return async (req, res, next) => {
+    try {
+      await requestHandler(req, res);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
