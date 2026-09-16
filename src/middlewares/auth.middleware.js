519e2e0f86a563ad8190f013999b1cbddb43f444
Ritesh Saxena <kingriteshsa@gmail.com>
2026-09-17 01:36:46 +0530

    feat: add JWT verification middleware for user authentication

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+const verifyJwt = asyncHandler(async (req, res, next) => {
+  const token =
+    req.cookies.accessToken ||
+    req.header("Authorization").replace("Bearer ", "");
+
+  if (!token) {
+    throw new ApiError(401, "Unauthorized: No token provided");
+  }
+
+  const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+  const user = await User.findById(decodedToken._id).select(
+    "-password -refreshToken",
+  );
+
+  if (!user) {
+    throw new ApiError(401, "Unauthorized: User not found");
+  }
+
+  req.user = user;
+  next();
+});
+
+export default verifyJwt;
